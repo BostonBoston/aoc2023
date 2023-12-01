b3bf853cbd4c2804801b0590bef4b342618ae634
Boston <37150222+BostonBoston@users.noreply.github.com>
2023-12-01 00:27:42 -0600

    day 1

---
+++ day 1/trebuchet.nim
@@ -0,0 +1,69 @@
+import strutils, times
+const input: string = slurp("./input.txt")
+
+const data: seq[string] = input.rsplit('\n')
+
+let t1 = cpuTime() 
+block part1:
+  var sum: int
+
+  for entry in data:
+    var first, last: char = 'a'
+    var dist: int = 0
+    for i, c in entry:
+      if c.isDigit: 
+        first = c
+        dist = i
+        break
+    for i in countdown(entry.len-1, dist):
+      if entry[i].isDigit:
+        last = entry[i]
+        break
+    try:
+      let num = parseInt(first & last)
+      sum = sum + num 
+    except: discard
+
+  echo sum
+
+const digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+block part2:
+  var sum: int
+
+  for entry in data:
+    var first, last: string = "a"
+    var dist: int = 0
+    block outer:
+      for i in 0..entry.len-1:
+        let span = entry[0..i]
+        for x, num in digits:
+          if span.contains(num):
+            first = $x
+            dist = i
+            break outer
+          if span.contains($x):
+            first = $x
+            dist = i
+            break outer
+    block outer:     
+      for i in countdown(entry.len-1, dist):
+        let span = entry[i..entry.len-1]
+        for x, num in digits:
+          if span.contains(num):
+            last = $x
+            break outer
+          if span.contains($x):
+            last = $x
+            break outer
+    try:
+      let num = parseInt(first & last)
+      sum = sum + num 
+    except: discard
+
+  echo sum
+let t2 = cpuTime()
+
+echo "This took " & $(t2-t1) & " seconds"
+
+    
