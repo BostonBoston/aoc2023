bc1ce84e056139dc1b55824f8516150f8f00ccb0
Boston <37150222+BostonBoston@users.noreply.github.com>
2023-12-04 20:24:21 -0600

    minimize day 4 solution

--- day 4/scratchcards.nim
+++ day 4/scratchcards.nim
@@ -2,35 +2,24 @@ import std/[strutils, math, sequtils, intsets, monotimes, times]
 const input = slurp("input.txt").rsplit('\n')
 let t1 = getMonoTime()
 
-type Card = object 
-  id, count: int
-  winningNumbers, ourNumbers: IntSet
-
-
 var 
-  cards: seq[Card] = newSeqOfCap[Card](input.len)
+  cards: seq[int] = newSeq[int](input.len)
   totalPoints, totalCards: int
-
-for i, line in input:
-  let nums = line.split(": ")[1].split(" | ")
-  var tWin, tOur: IntSet
-
-  tWin = nums[0].splitWhitespace.map(parseInt).toIntSet
-  tOur = nums[1].splitWhitespace.map(parseInt).toIntSet
-  cards.add Card(id: i+1, count: 1, winningNumbers: tWin, ourNumbers: tOur) 
 
 proc calcScore(n: int): int = 
   if n != 0: result = 2^(n-1)
 
-for card in cards:
-  let matchingNumbers = (card.winningNumbers * card.ourNumbers).len
-  totalPoints += matchingNumbers.calcScore
-  totalCards += card.count
-  for i in card.id+1..card.id+matchingNumbers: 
-    cards[i-1].count += card.count
-
+for i, line in input:
+  inc cards[i]
+  let nums = line.split(": ")[1].split(" | ")
+  let wins = (nums[0].splitWhitespace.map(parseInt).toIntSet * nums[1].splitWhitespace.map(parseInt).toIntSet).len
+  
+  totalPoints += wins.calcScore
+  totalCards += cards[i]
+  for j in i+1..i+wins: 
+    cards[j] += cards[i]
+  
 let t2 = getMonoTime()
-
 
 echo "Part1 ", totalPoints
 echo "Part2 ", totalCards
